a492ba7af2b5fccf4b9e1967f3344cb9b4d3c108
zhuwj <767839489@qq.com>
2019-08-28 21:43:42 +0800

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd'
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type='primary'>uhkj</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
